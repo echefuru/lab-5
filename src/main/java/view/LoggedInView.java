f77e2bfc4b24ae6856179d873e1718dc6de72809
estelleyay <wxiaoyi595@gmail.com>
2024-10-23 21:44:40 -0400

    Main.java and LoggedInView.java Xiaoyi Wang (Estelle)

--- src/main/java/view/LoggedInView.java
+++ src/main/java/view/LoggedInView.java
@@ -36,6 +36,7 @@ public class LoggedInView extends JPanel implements PropertyChangeListener {
     private final JTextField passwordInputField = new JTextField(15);
     private final JButton changePassword;
 
+    // @SuppressWarnings("checkstyle:UnusedLocalVariable")
     public LoggedInView(LoggedInViewModel loggedInViewModel) {
         this.loggedInViewModel = loggedInViewModel;
         this.loggedInViewModel.addPropertyChangeListener(this);
@@ -100,9 +101,11 @@ public class LoggedInView extends JPanel implements PropertyChangeListener {
                 // This creates an anonymous subclass of ActionListener and instantiates it.
                 evt -> {
                     if (evt.getSource().equals(logOut)) {
-                        // TODO: execute the logout use case through the Controller
+                        // execute the logout use case through the Controller
                         // 1. get the state out of the loggedInViewModel. It contains the username.
                         // 2. Execute the logout Controller.
+                        final LoggedInState currentState = loggedInViewModel.getState();
+                        logoutController.execute(currentState.getUsername());
                     }
                 }
         );
@@ -138,6 +141,7 @@ public class LoggedInView extends JPanel implements PropertyChangeListener {
     }
 
     public void setLogoutController(LogoutController logoutController) {
-        // TODO: save the logout controller in the instance variable.
+        // save the logout controller in the instance variable.
+        this.logoutController = logoutController;
     }
 }
